4868efa3acca3785a994e4903167ff88e12247d5
Tathagat Priyadarshi <100780858+priyadarshii786@users.noreply.github.com>
2024-03-22 00:39:51 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to count the number of possible triangles.
+    int findNumberOfTriangles(int arr[], int n)
+    {
+        // code here
+        // jai ganesh
+        
+        int ans=0;
+        sort(arr,arr+n,greater<int> ());
+        for(int i=0;i<n-2;i++){
+            int largerSide = arr[i];
+            int l=i+1,r=n-1;
+            while(l<r){
+                if(arr[l]+arr[r]>largerSide){
+                    ans+=r-l;
+                    l++;
+                }
+                else
+                r--;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.findNumberOfTriangles(arr,n) <<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
